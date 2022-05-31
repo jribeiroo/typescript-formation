dddb474948b422e4fdd1f966278be2ac300985e5
jribeiroo <nicolas.jnr10@outlook.com>
2022-05-30 23:10:52 -0300

    centralizando updates em um único lugar

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,16 +1,16 @@
 export abstract class View<T>  { // 'T' generics   // abstract não permite criar instancia a não ser herdada da filha
 
-protected elemento : HTMLInputElement;
+    protected elemento: HTMLInputElement;
 
-constructor(seletor: string){
-    this.elemento = document.querySelector(seletor);
-}
+    constructor(seletor: string) {
+        this.elemento = document.querySelector(seletor);
+    }
 
-update(model: T ): void {
-    const template = this.template(model);
-    this.elemento.innerHTML = template;
-}
+    public update(model: T): void {
+        const template = this.template(model);
+        this.elemento.innerHTML = template;
+    }
 
-protected abstract template(model: T): string;
-  
+    protected abstract template(model: T): string;
+
 }
--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -14,12 +14,9 @@ export class NegociacaoController {
     }
     adiciona() {
         const negociacao = this.criaNegociacao();
-        negociacao.data.setDate(12);
         this.negociacoes.adiciona(negociacao);
-        this.negociacoesView.update(this.negociacoes);
-        this.mensagemView.update('Negociação add com sucesso');
-        this.mensagemView;
         this.limparFormulario();
+        this.atualizaView();
     }
     criaNegociacao() {
         const exp = /-/g;
@@ -33,5 +30,9 @@ export class NegociacaoController {
         this.inputQuantidade.value = '';
         this.inputValor.value = '';
         this.inputData.focus();
+    }
+    atualizaView() {
+        this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update('Negociação add com sucesso');
     }
 }
