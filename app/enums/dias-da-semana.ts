91836a5665f58eacf0a4c3aa3466a52b99dedb3d
jribeiroo <nicolas.jnr10@outlook.com>
2022-06-06 23:31:58 -0300

    add enums

---
+++ app/enums/dias-da-semana.ts
@@ -0,0 +1,9 @@
+export enum DiasDaSemana { 
+    DOMINGO = 0,
+    SEGUNDA = 1,
+    TERCA = 2,
+    QUARTA = 3,
+    QUINTA = 5,
+    SEXTA = 6,
+    SABADO = 7
+}
---
+++ dist/js/enums/dias-da-semana.js
@@ -0,0 +1,10 @@
+export var DiasDaSemana;
+(function (DiasDaSemana) {
+    DiasDaSemana[DiasDaSemana["DOMINGO"] = 0] = "DOMINGO";
+    DiasDaSemana[DiasDaSemana["SEGUNDA"] = 1] = "SEGUNDA";
+    DiasDaSemana[DiasDaSemana["TERCA"] = 2] = "TERCA";
+    DiasDaSemana[DiasDaSemana["QUARTA"] = 3] = "QUARTA";
+    DiasDaSemana[DiasDaSemana["QUINTA"] = 5] = "QUINTA";
+    DiasDaSemana[DiasDaSemana["SEXTA"] = 6] = "SEXTA";
+    DiasDaSemana[DiasDaSemana["SABADO"] = 7] = "SABADO";
+})(DiasDaSemana || (DiasDaSemana = {}));
